52a6adc343fdb6c2c9386970e7dbb1914ab45fec
Constantine Zubarev <kv.zubarev@gmail.com>
2022-07-06 21:37:24 +0600

    feat: base implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next'
+import { availableLanguages } from './i18n'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const {t, i18n} = useTranslation()
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,8 +20,13 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {t('learnReact')}
         </a>
+        <select defaultValue={i18n.language} onChange={(e) => i18n.changeLanguage(e.target.value)}>
+          {availableLanguages.map((language) => (
+            <option key={language}>{language}</option>
+          ))}
+        </select>
       </header>
     </div>
   );
