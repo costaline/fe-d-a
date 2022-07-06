ee9ff0cda767b15f7c5f9f5a53578698ad8c71eb
Constantine Zubarev <kv.zubarev@gmail.com>
2022-07-06 23:11:13 +0600

    feat: emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
 import React from 'react';
 import { useTranslation } from 'react-i18next'
 import { availableLanguages } from './i18n'
 import logo from './logo.svg';
 import './App.css';
 
+const Select = styled.select`
+  margin: 20px
+`
+
+const padding = css`padding: 10px 20px`
+
 function App() {
   const {t, i18n} = useTranslation('main')
 
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p css={css`border: 2px dashed red; ${padding};`}>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
@@ -22,11 +30,11 @@ function App() {
         >
           {t('learnText')}
         </a>
-        <select defaultValue={i18n.language} onChange={(e) => i18n.changeLanguage(e.target.value)}>
+        <Select defaultValue={i18n.language} onChange={(e) => i18n.changeLanguage(e.target.value)}>
           {availableLanguages.map((language) => (
             <option key={language}>{language}</option>
           ))}
-        </select>
+        </Select>
       </header>
     </div>
   );
