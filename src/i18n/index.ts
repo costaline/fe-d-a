b189f90740218cf66b5c7f87473a0003b9ecc333
Constantine Zubarev <kv.zubarev@gmail.com>
2022-07-08 21:16:01 +0600

    lint(es): fix warnings

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,25 +1,27 @@
-import i18n from "i18next";
+import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 import resourcesToBackend from 'i18next-resources-to-backend'
-import {initReactI18next} from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from 'react-i18next'
 
 export const availableLanguages = ['en', 'ru']
 
 i18n
-  .use(initReactI18next)
-  .use(resourcesToBackend((language, namespace, callback) => {
-    import(`./${language}/${namespace}.json`)
-      .then((resources) => {
-        callback(null, resources)
-      })
-      .catch((error) => {
-        callback(error, null)
-      })
-  }))
-  .use(LanguageDetector)
-  .init({
-    debug: process.env.NODE_ENV === 'development',
-    defaultNS: "main",
-    ns: ['main'],
-    fallbackLng: "en",
-  });
+	.use(initReactI18next)
+	.use(
+		resourcesToBackend((language, namespace, callback) => {
+			import(`./${language}/${namespace}.json`)
+				.then((resources) => {
+					callback(null, resources)
+				})
+				.catch((error) => {
+					callback(error, null)
+				})
+		})
+	)
+	.use(LanguageDetector)
+	.init({
+		debug: process.env.NODE_ENV === 'development',
+		defaultNS: 'main',
+		ns: ['main'],
+		fallbackLng: 'en',
+	})
