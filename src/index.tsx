3bd8af584219249eba8a2da04293ea79d1d2bf7b
Constantine Zubarev <kv.zubarev@gmail.com>
2022-07-11 18:01:44 +0600

    feat: pwa
    * unregistered

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client'
 
 import { Root } from '@@/Root'
 import reportWebVitals from './reportWebVitals'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 import 'sanitize.css'
 import 'sanitize.css/forms.css'
@@ -15,6 +16,11 @@ import './assets/styles/global.scss'
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(<Root />)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
